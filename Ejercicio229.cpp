114214fd1fd478e20fbff4ae92952674508ec62f
DRpin2010 <rpin@alumno.montepiedra.edu.ec>
2025-10-13 13:10:29 -0500

    Add files via upload

---
+++ Ejercicio229.cpp
@@ -0,0 +1,12 @@
+// Ejercicio 229
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+
+cout << "Sistema ACL demo: permisos representados en bitmask\n";
+
+    return 0;
+}
---
+++ Ejercicio234.cpp
@@ -0,0 +1,14 @@
+// Ejercicio 234
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int main() {
+
+cout << "Parser booleano simple: evalua 'true' o 'false' ingresado\n";
+string s; cin >> s;
+cout << ((s=="true")?"Verdadero\n":"Falso\n");
+
+    return 0;
+}
