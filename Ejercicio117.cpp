6e552801c2cb4b7dc7cc383d2b6818e813b4517e
DRpin2010 <rpin@alumno.montepiedra.edu.ec>
2025-10-14 21:51:13 -0500

    Add files via upload

---
+++ Ejercicio117.cpp
@@ -0,0 +1,18 @@
+// Ejercicio117.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double c;
+    cout << "Ejercicio 117: Ingrese grados Celsius: ";
+    if(!(cin>>c)) return 0;
+    cout << "Fahrenheit: " << (c * 9.0/5.0 + 32.0) << "\n";
+
+    return 0;
+}
