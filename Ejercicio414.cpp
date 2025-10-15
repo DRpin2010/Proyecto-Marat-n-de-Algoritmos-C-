fb50f138ba44eada1e2ac4affbd06eb693b880b4
DRpin2010 <rpin@alumno.montepiedra.edu.ec>
2025-10-14 22:34:58 -0500

    Add files via upload

---
+++ Ejercicio414.cpp
@@ -0,0 +1,15 @@
+// Ejercicio414.cpp
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    cout << "Ejercicio 414: BST implementacion completa es larga; este es un stub.\n";
+
+    return 0;
+}
